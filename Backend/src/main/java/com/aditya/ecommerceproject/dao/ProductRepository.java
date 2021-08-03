7fced5ad91fcd4e105837fdac9e67286f9b6981a
AdityaThadeshwar <aditya.thadeshwar@gmail.com>
2021-08-03 21:52:39 +0530

    CrossOrigin support to allow angular application to make calls to spring

--- Backend/src/main/java/com/aditya/ecommerceproject/dao/ProductRepository.java
+++ Backend/src/main/java/com/aditya/ecommerceproject/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.aditya.ecommerceproject.dao;
 
 import com.aditya.ecommerceproject.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200") //Allow angular to make calls to spring application
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
