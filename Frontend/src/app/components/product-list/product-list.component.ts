df74a6ad6486058ea1ce45fcd2c30a8695ddb6a3
AdityaThadeshwar <aditya.thadeshwar@gmail.com>
2021-08-05 21:05:17 +0530

    Define router outlet

--- Frontend/src/app/components/product-list/product-list.component.ts
+++ Frontend/src/app/components/product-list/product-list.component.ts
@@ -1,28 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ProductService} from "../../services/product.service";
 import {Product} from "../../common/product";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list-grid.component.html',
-  //templateUrl: './product-list-table.component.html',
-  //templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+	selector: 'app-product-list',
+	templateUrl: './product-list-grid.component.html',
+	styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
-  products: Product[] | undefined;
-  constructor(private productService: ProductService) { }
+	products: Product[] | undefined;
+	currentCategoryId: number | undefined;
 
-  ngOnInit(): void {
-    this.listProducts();
-  }
+	constructor(private productService: ProductService,
+				private route: ActivatedRoute) { //Injects route. Useful for accessing route parameters
+	}
 
-  listProducts() {
-    this.productService.getProductList().subscribe(   //method is invoked once you subscribe
-      data => {
-        this.products = data; //assigns result to product array
-      }
-    )
-  }
+	ngOnInit(): void {
+		this.route.paramMap.subscribe(() => {    //anytime the parameter value changes and even if the navigation is being done on the same component, paramMap observable will detect and read it
+			this.listProducts()
+		});
+	}
+
+	listProducts() {
+
+		const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+		//Convert id from string to number
+		if(hasCategoryId) {
+			this.currentCategoryId = Number(this.route.snapshot.paramMap.get('id'));
+		}
+
+		else
+			this.currentCategoryId = 1;
+
+		//Get products for given category id
+		this.productService.getProductList(this.currentCategoryId).subscribe(   //method is invoked once you subscribe
+			data => {
+				this.products = data; //assigns result to product array
+			}
+		)
+	}
 }
