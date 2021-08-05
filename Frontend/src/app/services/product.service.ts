f55863ce9cb2266d646bd7a74e783868fc771766
AdityaThadeshwar <aditya.thadeshwar@gmail.com>
2021-08-05 21:29:32 +0530

    Define REST url endpoint based on category id

--- Frontend/src/app/services/product.service.ts
+++ Frontend/src/app/services/product.service.ts
@@ -16,7 +16,11 @@ export class ProductService {
 
   //Get json response and store it in an array
   getProductList(theCategoryId: number): Observable<Product[]> {
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+
+  	//url based on category id
+  	const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
