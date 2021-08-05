a0e2d2c1d65ed2df82ce517e435157bf017b806d
AdityaThadeshwar <aditya.thadeshwar@gmail.com>
2021-08-05 21:22:29 +0530

    Query Param findByCategoryId

--- Backend/src/main/java/com/aditya/ecommerceproject/dao/ProductRepository.java
+++ Backend/src/main/java/com/aditya/ecommerceproject/dao/ProductRepository.java
@@ -1,9 +1,17 @@
 package com.aditya.ecommerceproject.dao;
 
 import com.aditya.ecommerceproject.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200") //Allow angular to make calls to spring application
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    //Query method findBy: select * from products where category_id = @id
+    //Spring REST will automatically expose this endpoint
+    //http://localhost:8080/api/products/search/findByCategoryId?id=?
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
