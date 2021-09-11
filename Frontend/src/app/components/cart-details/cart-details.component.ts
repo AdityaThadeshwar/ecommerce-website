bb8856ce16a0834ab9316e983e344f915175b705
AdityaThadeshwar <aditya.thadeshwar@gmail.com>
2021-09-11 19:23:17 +0530

    Added new method listCartDetails

--- Frontend/src/app/components/cart-details/cart-details.component.ts
+++ Frontend/src/app/components/cart-details/cart-details.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {CartItem} from "../../common/cart-item";
+import {CartService} from "../../services/cart.service";
 
 @Component({
   selector: 'app-cart-details',
@@ -7,9 +9,33 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CartDetailsComponent implements OnInit {
 
-  constructor() { }
+	cartItems: CartItem[] = [];
+	totalQuantity: number = 0;
+	totalPrice: number = 0;
+
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+  	this.listCartDetails();
   }
 
+	listCartDetails() {
+
+  		//Get a handle on cart items
+		this.cartItems = this.cartService.cartItems;
+
+		//Subscribe to cart totalPrice
+		this.cartService.totalPrice.subscribe(
+			data => this.totalPrice = data
+		);
+
+		//subscribe to cart totalQuantity
+		this.cartService.totalQuantity.subscribe(
+			data => this.totalQuantity = data
+		);
+
+		//compute cart total price and quantity
+		this.cartService.computeCartTotals();
+
+	}
 }
