a19b850264ee3da239fcf7a304b7990e36b2208d
AdityaThadeshwar <aditya.thadeshwar@gmail.com>
2022-01-09 16:56:58 +0530

    Added AuthInterceptor - to add authorization token for the user before making request to /api/orders

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {ProductListComponent} from './components/product-list/product-list.component';
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {ProductService} from "./services/product.service";
 import {Router, RouterModule, Routes} from "@angular/router";
 import {ProductCategoryMenuComponent} from './components/product-category-menu/product-category-menu.component';
@@ -24,6 +24,7 @@ import {OKTA_CONFIG, OktaAuthGuard, OktaAuthModule, OktaCallbackComponent} from 
 import myAppConfig from './config/my-app-config';
 import {MembersPageComponent} from './components/members-page/members-page.component';
 import {OrderHistoryComponent} from './components/order-history/order-history.component';
+import {AuthInterceptorService} from "./services/auth-interceptor.service";
 
 //Create an object onAuthRequired, when user tries to login and they haven't been authenticated, route them to login page
 const oktaConfig = Object.assign({
@@ -75,7 +76,8 @@ const routes: Routes = [
 		ReactiveFormsModule,
 		OktaAuthModule
 	],
-	providers: [ProductService, {provide: OKTA_CONFIG, useValue: oktaConfig}],
+	providers: [ProductService, {provide: OKTA_CONFIG, useValue: oktaConfig},
+				{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
 	bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ Frontend/src/app/services/auth-interceptor.service.ts
@@ -0,0 +1,44 @@
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {from, Observable} from "rxjs";
+import {OktaAuthService} from "@okta/okta-angular";
+
+@Injectable({
+	providedIn: 'root'
+})
+export class AuthInterceptorService implements HttpInterceptor {
+
+	constructor(private oktaAuth: OktaAuthService) {
+	}
+
+	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+		return from(this.handleAccess(request, next));
+	}
+
+	private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
+
+		//Only add an access token for secured endpoints
+		const securedEndpoints = ['http://localhost:8080/api/orders'];
+
+		console.log('inside handleAccess')
+
+		//check if url matches with above array
+		if(securedEndpoints.some(url => request.urlWithParams.includes(url))) {
+
+			console.log('securedEndpoints matched')
+
+			//get access token
+			const accessToken = await this.oktaAuth.getAccessToken();
+
+			//close request and add new access token. Clone because request is immutable
+			request = request.clone( {
+				setHeaders: {
+					Authorization: 'Bearer ' + accessToken
+				}
+			});
+		}
+
+		//Continue with other interceptors that are in the chain. If none then make call to the given resp api
+		return next.handle(request).toPromise();
+	}
+}
