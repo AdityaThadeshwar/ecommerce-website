7f9daafe4111642391db34520215f4ac117beb31
AdityaThadeshwar <aditya.thadeshwar@gmail.com>
2022-01-09 16:13:23 +0530

    Secure Orders to authenticated user. Added okta authorization to backend

---
+++ Backend/src/main/java/com/aditya/ecommerceproject/config/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.aditya.ecommerceproject.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        //protect endpoint /api/orders
+        http.authorizeRequests().antMatchers("/api/orders/**").authenticated().and().oauth2ResourceServer().jwt();
+
+        //add cors filters
+        http.cors();
+
+        //response body for 401 unauthorized
+        Okta.configureResourceServer401ResponseBody(http);
+    }
+}
