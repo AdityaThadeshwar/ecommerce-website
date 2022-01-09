b1658e49b3d44401b8d25994744d7090a0528b28
AdityaThadeshwar <aditya.thadeshwar@gmail.com>
2022-01-09 18:26:32 +0530

    HTTPS Support. Added keystore and required properties

--- Frontend/src/app/config/my-app-config.ts
+++ Frontend/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
 	oidc: {
 		clientId: '0oa2bwf4peLWW8LLF5d7',
 		issuer: 'https://dev-9900891.okta.com/oauth2/default',
-		redirectUri: 'http://localhost:4200/login/callback',
+		redirectUri: 'https://localhost:4200/login/callback',
 		scopes: ['openid', 'profile', 'email']
 	}
 }
