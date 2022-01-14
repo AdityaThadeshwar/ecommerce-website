d7ffde0b5f9249316f2d81a6020b77c4927d456a
AdityaThadeshwar <aditya.thadeshwar@gmail.com>
2022-01-14 13:50:30 +0530

    Create endpoint for PaymentIntent

--- Backend/src/main/java/com/aditya/ecommerceproject/controller/CheckoutController.java
+++ Backend/src/main/java/com/aditya/ecommerceproject/controller/CheckoutController.java
@@ -1,10 +1,18 @@
 package com.aditya.ecommerceproject.controller;
 
+import com.aditya.ecommerceproject.dto.PaymentInfo;
 import com.aditya.ecommerceproject.dto.Purchase;
 import com.aditya.ecommerceproject.dto.PurchaseResponse;
 import com.aditya.ecommerceproject.service.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/checkout")
@@ -23,5 +31,16 @@ public class CheckoutController {
         PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
 
         return purchaseResponse;
+    }
+
+    // ResponseEntity represents the whole HTTP response: status code, headers, and body.
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+
+        PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+
+        String paymentStr = paymentIntent.toJson();
+
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
     }
 }
