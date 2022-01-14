c4cb48934508c97a14c00a83ea3085a54c7a02f1
AdityaThadeshwar <aditya.thadeshwar@gmail.com>
2022-01-14 13:42:02 +0530

    Stripe dependency added with DTO

---
+++ Backend/src/main/java/com/aditya/ecommerceproject/dto/PaymentInfo.java
@@ -0,0 +1,10 @@
+package com.aditya.ecommerceproject.dto;
+
+import lombok.Data;
+
+@Data
+public class PaymentInfo {
+
+    private int amount;
+    private String currency;
+}
--- Backend/src/main/java/com/aditya/ecommerceproject/service/CheckoutService.java
+++ Backend/src/main/java/com/aditya/ecommerceproject/service/CheckoutService.java
@@ -1,9 +1,14 @@
 package com.aditya.ecommerceproject.service;
 
+import com.aditya.ecommerceproject.dto.PaymentInfo;
 import com.aditya.ecommerceproject.dto.Purchase;
 import com.aditya.ecommerceproject.dto.PurchaseResponse;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 
 public interface CheckoutService {
 
     PurchaseResponse placeOrder(Purchase purchase);
+
+    PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException;
 }
